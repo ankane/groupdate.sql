1922a687bab4754c5fc5e5ddf6294d7633edb001
Andrew Kane <acekane1@gmail.com>
2015-09-17 10:00:29 -0700

    Removed pg_typecast

--- Gemfile
+++ Gemfile
@@ -3,5 +3,4 @@ source 'https://rubygems.org'
 gem "rake"
 gem "minitest"
 gem "pg"
-gem "pg_typecast"
 gem "mysql2"
--- test/postgresql_test.rb
+++ test/postgresql_test.rb
@@ -9,6 +9,7 @@ class TestPostgresql < Minitest::Test
         conn = PG.connect(:dbname => "groupdate_test")
         conn.query File.read("postgresql.sql")
         conn.query "SET time zone 'UTC'"
+        conn.type_map_for_results = PG::BasicTypeMapForResults.new(conn)
         conn
       end
   end
